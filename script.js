201f0565582e772214ec61d76ddf528615fa7b7c
salinasedwin890-lgtm <salinasedwin890@gmail.com>
2026-04-23 07:34:03 -0600

    xd

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-function MOSTRARMENSAJE() {
+ /*function MOSTRARMENSAJE () {
 
-let mensaje = document.getElementById("mensaje");
+    let mensaje = document.getElementById("mensaje");
 
-mensaje.innerHTML= "cuida y protege a los animales";
-mensaje.style.display="block";
+    mensaje.innerHTML = "cuida y protege a los animales";
+    mensaje.style.display ="block";
 
-setTimeout(()  => {
- mensaje.style.opacity="i";
- mensaje.style.transform= "translatey (0)";
-},50);
+ setTimeout(()  => {
+    mensaje.style.opacity = "i";
+    mensaje.style.transform = "translatey (0)";
+},50); */ 
 
+function MOSTRARMENSAJE () {
+    let mensaje = document.getElementById("mensaje");
 
+    mensaje.innerHTML = "cuida y protege a los animales";
+    mensaje.style.display = "block";
 
-
-
-
-
+    setTimeout(() => {
+        mensaje.style.opacity = "1";
+        mensaje.style.transform = "translateY(0)";
+    }, 50);
 }
+
+
+
+
+
+
+
+
 
 
 
