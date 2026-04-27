a4c2e648afdaa004055ab44d0ae3b80ba8561f18
salinasedwin890-lgtm <salinasedwin890@gmail.com>
2026-04-27 14:44:25 -0600

    aCTUALIZADA

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function MOSTRARMENSAJE () {
     let mensaje = document.getElementById("mensaje");
 
-    mensaje.innerHTML = "Hola mi nombre es Edwin Emilio Salinas Salinas tengo 16 años estudio en el CECyte EMSaD 35 mi mi pasatiempo favorito es ensayar musica";
+    mensaje.innerHTML = "cuida y protege a los animales";
     mensaje.style.display = "block";
 
     setTimeout(() => {
@@ -10,6 +10,16 @@ function MOSTRARMENSAJE () {
     }, 50);
 }
 
+    function MOSTRARMENSAJE2() {
+    let mensaje = document.getElementById("mensaje2");
+    mensaje.innerHTML = "Hola mi nombre es Edwin Emilio Salinas Salinas tengo 16 años estudio en el CECyte EMSaD 35 mi mi pasatiempo favorito es ensayar musica";
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+        mensaje.style.opacity = "1";
+        mensaje.style.transform = "translateY(0)";
+    }, 50);
+}
 
 
 
