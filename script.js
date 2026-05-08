c5ef695ef6b15f104b2e37d731c4187229ae00de
salinasedwin890-lgtm <salinasedwin890@gmail.com>
2026-05-08 07:58:12 -0600

    actualizado

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-
 function MOSTRARMENSAJE() {
     let mensaje = document.getElementById("mensaje");
 
-    // OCULTAR EL BOTÓN
-    boton.style.display = "none"; 
+    if (mensaje.style.display === "block") {
+        // Ocultar mensaje
+        mensaje.style.opacity = "0";
+        mensaje.style.transform = "translateY(-20px)";
 
-    mensaje.innerHTML = "cuida y protege a los animales";
-    mensaje.style.display = "block";
+        setTimeout(() => {
+            mensaje.style.display = "none";
+        }, 300);
 
-    setTimeout(() => {
-        mensaje.style.opacity = "1";
-        mensaje.style.transform = "translateY(0)";
-    }, 50);
+    } else {
+        // Mostrar mensaje
+        mensaje.innerHTML = "cuida y protege a los animales";
+        mensaje.style.display = "block";
+
+        setTimeout(() => {
+            mensaje.style.opacity = "1";
+            mensaje.style.transform = "translateY(0)";
+        }, 50);
+    }
 }
 
-function MOSTRARMENSAJE2(boton) {
+function MOSTRARMENSAJE2() {
     let mensaje = document.getElementById("mensaje2");
 
-    // OCULTAR EL BOTÓN
-    boton.style.display = "none";
+    if (mensaje.style.display === "block") {
+        // Ocultar mensaje
+        mensaje.style.opacity = "0";
+        mensaje.style.transform = "translateY(-20px)";
 
-    mensaje.innerHTML = "Hola mi nombre es Edwin Emilio Salinas Salinas tengo 16 años estudio en el CECyte EMSaD 35 mi mi pasatiempo favorito es ensayar musica";
-    mensaje.style.display = "block";
+        setTimeout(() => {
+            mensaje.style.display = "none";
+        }, 300);
 
-    setTimeout(() => {
-        mensaje.style.opacity = "1";
-        mensaje.style.transform = "translateY(0)";
-    }, 50);
+    } else {
+        // Mostrar mensaje
+        mensaje.innerHTML = "Hola mi nombre es Edwin Emilio Salinas Salinas tengo 16 años estudio en el CECyte EMSaD 35 y mi pasatiempo favorito es ensayar música";
+        mensaje.style.display = "block";
+
+        setTimeout(() => {
+            mensaje.style.opacity = "1";
+            mensaje.style.transform = "translateY(0)";
+        }, 50);
+    }
 }
 
 
